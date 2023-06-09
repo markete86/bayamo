e7da00703730abf9974e908159cbee827d638ad0
Marcos <markete86@gmail.com>
2023-06-09 01:09:01 -0300

    Create Avioncito Component

---
+++ src/app/avioncito/avioncito.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-avioncito',
+  templateUrl: './avioncito.component.html',
+  styleUrls: ['./avioncito.component.scss']
+})
+export class AvioncitoComponent {
+
+}
